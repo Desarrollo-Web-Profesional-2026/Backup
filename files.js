3896531a1ae4ab6bd72f78e03cd64b5310efa3cc
LiizethRR <ramirezramirezlizeth900@gmail.com>
2026-02-02 16:12:51 -0600

    Segundo desafio

---
+++ files.js
@@ -0,0 +1,21 @@
+
+//Desafio 2 
+import { readFile, writeFile } from 'node:fs/promises'
+import { existsSync } from 'node:fs'
+
+
+let usuarios = []
+
+try {
+  if (existsSync('usuarios.json')) {
+    const data = await readFile('usuarios.json')
+    usuarios = JSON.parse(data)
+  }
+} catch {}
+
+usuarios.push({ nombre: 'Adim Ondra', email: 'adam@climb.ing' })
+
+await writeFile('usuarios.json', JSON.stringify(usuarios, null, 2))
+console.log(usuarios)
+
+
